eab438af837942eaef283caa5104c966010fc826
Eureka Code <batagapaul10@gmail.com>
2025-03-05 11:20:16 +0800

    Added deploy script

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/GECO-Download-Page/', // Use your actual repository name here
   plugins: [react(), tailwindcss()],
 })
